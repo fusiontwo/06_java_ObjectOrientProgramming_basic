62f584f4701e8e10af47f9117afd3b84d1dcafe9
fursiontwo <yr0955@naver.com>
2023-09-04 20:43:12 +0900

    Commit 230904

---
+++ 06_java_ObjectOrientProgramming_basic/src/step6_02/method/MethodEx04.java
@@ -0,0 +1,22 @@
+package step6_02.method;
+
+// 다양한 변수의 종류
+class VariableEx {
+	
+	int a;  // 객체 변수, 인스턴스 변수(객체에서 사용하는 변수)
+//	static int b;  // 클래스 변수, 정적 변수(클래스에서 사용하는 변수)
+	
+	void testVar(int c) {  // 매개 변수(메서드로 전달되는 데이터를 받을 때 사용하는 변수)
+		int d;  // 지역 변수(메서드 내부에서 사용하는 변수)
+	}
+}
+
+public class MethodEx04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
